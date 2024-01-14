9a8d9deaa458a81f72695c2773a0c0cd3f1c9787
Michael Ketchel <michael.ketchel@gmail.com>
2024-01-14 18:36:03 -0500

    Fixed door

--- door.gd
+++ door.gd
@@ -1,5 +1,10 @@
 extends CSGBox3D
 
+func on_interact(interactor, interaction_raycast) -> bool:
+	print("%s touched by %s" % [self, interactor])
+	#print("Material: %s" % self.get_material())
+	open()
+	return true
 
 func open() -> void:
 	print("opened door")
